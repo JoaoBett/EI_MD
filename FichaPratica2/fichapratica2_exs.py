a26538c60fa035d99d0d2962eca8ee0684ae6bf2
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-03-08 12:55:15 +0000

    feat: ex3

--- FichaPratica2/fichapratica2_exs.py
+++ FichaPratica2/fichapratica2_exs.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 #a)
 def f(x):
-    out = mt.cos(x) + mt.e**x
+    out = (mt.cos(x) + (mt.e**x))
     return out
 
 #b)
@@ -26,36 +26,61 @@ c = np.linspace(1, 2,98)
 
 print(c)
 
+##################################Fazer D e E errado
 #d)
-d = f(x)
+#d = f(x)
 #print(D)    #x is not defined
 
 #e)
-d = np.linspace(0, 100,50)
+x = np.linspace(-2, 2)
+
+#plt.plot(x,f(x))
+plt.show()
 
 #%%
 #EX 2
 
-def f(x):
+def g(x):
     out = ((x**5)-(3*x**4)-(3*x**3)+(7*x**2)+(6*x))
     return out
 
+H = g(-1)
 
 plt.plot(H)
 plt.show()
 
 #%%
 #EX 3
+def r(x,y):
+    out = (y*((0.5)**(x/140)))
+    return out
+    
+e = r(x,10)    
 
-
-
-
+plt.plot(e)
+plt.show()
 #%%
 #EX 4
 
+#a)
+def z(n):
+    out = 0    
+    return out
 
-
-
+#b)
 
 #%%
 #EX 5
+
+#a)
+
+
+
+
+#b)
+
+
+
+
+
+
