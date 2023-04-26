b1f33a6c02b09ed5ecd0f742a84cf95baac6aa39
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-04-26 09:53:13 +0100

    feat: aula-26-04

--- FIchaPratica5/aula.py
+++ FIchaPratica5/aula.py
@@ -61,5 +61,130 @@ else:
 #Ficha5
 
 #Relacoes
+import numpy as np
+
+#M = ([a1,a2,...],[b1,b2,...])
+
+#multiplicar matrizes
+#A@B
+
+#Potenciação de matrizes (A**3=[aij**3])
+
+#A^3 = A@A@A
+
+#np.linalg.matrix_power(A, 3)
+ 
+#Aceder a elementos da matriz
+
+#M[i,j]
+
+#M[1,2] --> aceder ao elementos que esta na linha 2 e na coluna 3
+
+#M[:,3] --> toda a quarta coluna
+
+#M[2,:] 
+
+#M[:,1:4] --> parte da 3 linha compreendida entre a segunda coluna e a 4 coluna(exclusive) 
+
+
+
+
+#Matrizes Especiais
+
+I4=np.eye(4)
+
+O=np.ones((L,C))
+Z=np.zeros((L,C))
+ 
+
+#Dimensões da matriz
+
+O.shape
+
+#Transposta da Matriz
+
+O.T
+
+#%%
+#Ficha 5 - Exs
+
+#1
+#a)
+MR=np.array([[1,0,1,0],[1,0,1,0],[1,1,0,0]])
+
+#b)
+
+MR_inv=MR.T
+
+#c)é uma especie do produto MR^2
+#RoR
+
+MR2=MR@MR
+
+def um(M):
+    #percorrer a matriz e se encontrar algum elemento maior que 1 torna-lo num 1
     
+    L,C=M.shape
+    for i in range(0,L):
+        for j in range(0,C):
+            if M[i,j]>1:
+                M[i,j]=1
+    return M
+
+MR2=um(MR2)
+
+MR3=MR@MR@MR #ou
+MR3=np.linalg,matrix_power(MR,3)
+MR3=um(MR3)  #ou
+MR3[MR3>1]=1
+
+
+#%%
+#ex 4
+#a)
+
+def is_reflexiva(M):
+    for i in range(len(M)):
+        
+        if M[i][i] != 1:
+            return False
+        
+        for j in range(len(M)):
+            if i!=j and M[i][j]!=0:
+                return False
+        
+    return True;
+
+#2 maneira de fazer
+def is_reflex(M):
+    L,C=M.Shape
+    logico=True
     
+    for i in range(0,L):
+        if M[i,i]==0:
+            return False
+    
+    return logico
+
+#3 maneira de fazer
+def is_reflexiv(M):
+    L,C=M.shape
+    return np.trace(M)==L
+
+#4 maneira de fazer
+def is_refle(M):
+    L,C=M.shape
+    return (np.diag(M)==np.diag(np.eye(L))).all()
+
+#b)
+def is_simetrica(M):
+    L,C=shape(M)
+    for i in range(0,L):
+        for j in range(i+1,C):
+            if M[i,j]!=M[j,i]:
+                return False
+    return True
+
+
+def is_simet(M):
+    return (M==M.T).all()
